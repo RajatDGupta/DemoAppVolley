e5db0f7cb4710bc37f9aaebfddcbfa8886b2f2b5
neeraj <ngupta.gupta53@gmail.com>
2017-12-15 15:29:07 +0530

    hi

---
+++ app/src/main/java/com/example/neeraj/demoapp/Some.java
@@ -0,0 +1,13 @@
+package com.example.neeraj.demoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Some extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_some);
+    }
+}
